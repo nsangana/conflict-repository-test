b1b6623a2f578c4ef061c26724bf22024fa76b20
naveen.sangana@databricks.com <naveen.sangana@databricks.com>
2023-05-12 21:44:51 +0000

    push
    initial push

---
+++ Main Branch Notebook.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC #This is the main notebook in the main branch
+
+# COMMAND ----------
+
+df = spark.range(1000)
+
+df.count()
+
+# COMMAND ----------
+
+
